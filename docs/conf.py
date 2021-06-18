9dcd8e4bed83022281394cea24eb4bff4186901b
Tony Cannistra <tony.cannistra@gmail.com>
2021-06-18 16:15:03 -0700

    use `audodoc_mock_imports` to deal with missing build deps

--- docs/conf.py
+++ docs/conf.py
@@ -48,3 +48,15 @@ html_show_sphinx = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Autodoc options
+autodoc_mock_imports = [
+    "tqdm",
+    "shapely",
+    "gdal",
+    "lxml",
+    "psycopg2",
+    "pyproj",
+    "rtree",
+    "osmium",
+]
